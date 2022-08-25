3db466ce4516c2aff0bedcec8727a1683a7317aa
joao victor <joaovictor31rodrigues1@gmail.com>
2022-08-25 17:40:22 -0300

    quinto commit

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,7 +3,7 @@ let idade = '20 anos'
 let email = "joaovictor31rodrigues1@gmail.com"
 let profissao = "desempregado" 
 let lugarDosSonhos = 'EstadosUnidos'
-let mensagem = "ola,joaoVictor Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para EstadosUnidos  Confirma pra gente alguns dados? A sua idade é ? 20 anos você é desempregado e o seu email é joaovictor31rodrigues1@gmail.com Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+let mensagem = "ola, joaoVictor Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para EstadosUnidos  Confirma pra gente alguns dados? A sua idade é ? 20 anos você é desempregado e o seu email é joaovictor31rodrigues1@gmail.com Ficamos no aguardo. Muito obrigado e boa viagem! :D"
 console.log(nome)
 console.log(idade)
 console.log(email)
---
+++ tarefa-dia-2/pratica-2.js
@@ -0,0 +1,84 @@
+// Este é um comentário no código. Comentários não são cumpridos, eles servem pra gente deixar alguma mensagem
+// para outras pessoas que sabem o código. Agora vamos para o exercício.
+
+// Analise os 10 comandos 'if' seguir e tentar prever qual mensagem será exibida na tela.
+// Fique à vontade de rodar o código para verificar se suas respostas estão corretas.
+
+seja  a  =  10
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):if
+if  ( a  ==  10 )  {
+    consola . log ( "Verdade! O valor" ,  a ,  "é igual a 10" ) ;
+}  senão  {
+    consola . log ( "Falso! O valor" ,  a ,  "não é igual a 10" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):if
+se  ( a  >  10 )  {
+    consola . log ( "Verdade! O valor" ,  a ,  "é maior que 10" ) ;
+}  senão  {
+    consola . log ( "Falso! O valor" ,  a ,  "não é maior 10" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):if
+if  ( 10  <=  a )  {
+    consola . log ( "Verdade! O valor" ,  a ,  "é maior ou igual a 10" ) ;
+}  senão  {
+    consola . log ( "Falso! O valor" ,  a ,  "não é maior ou igual a 10" ) ;
+}
+
+seja  b  =  verdadeiro
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):if
+if  ( b  ==  verdadeiro )  {
+    consola . log ( "Verdade! O valor" ,  b ,  "é igual a true" ) ;
+}  senão  {
+    consola . log ( "Falso! O valor" ,  b ,  "não é igual a true" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):else
+se  ( b )  {
+    consola . log ( "Verdade! O valor" ,  b ,  "é considerado como verdade" ) ;
+}  senão  {
+    consola . log ( "Falso! O valor" ,  b ,  "não é considerado como verdade" ) ;
+}
+
+seja  c  =  [ ]
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):else
+se  ( c )  {
+    consola . log ( "Verdade! O valor" ,  c ,  "é considerado como verdade" ) ;
+}  senão  {
+    consola . log ( "Falso! O valor" ,  c ,  "não é considerado como verdade" ) ;
+}
+
+seja  d  =  [ 1 , 2 , 3 , 4 ]
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):else
+se  ( d )  {
+    consola . log ( "Verdade! O valor" ,  d ,  "é considerado como verdade" ) ;
+}  senão  {
+    consola . log ( "Falso! O valor" ,  d ,  "não é considerado como verdade" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):if
+se  ( d [ 0 ]  ==  1 )  {
+    consola . log ( "Verdade! O valor" ,  d [ 0 ] ,  "do elemento do índice 0 da variável 'd'é igual a 1" ) ;
+} senão  {
+    consola . log ( "Falso! O valor" ,  d [ 0 ] ,  "do elemento do índice 0 da variável 'd'não é igual a 1" ) ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):if
+if  ( d [ 1 ]  ==  1 )  {
+    consola . log ( "Verdade! O valor" ,  d [ 1 ] ,  "do elemento do índice 1 da variável 'd'é igual a 1" ) ;
+}  senão  {
+    consola . log ( "Falso! O valor" ,  d [ 1 ] ,  "do elemento do índice 1 da variável 'd'não é igual a 1" ) ;
+}
+
+deixe  e  =  "Nome"
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):else
+if  ( typeof ( e )  ==  'string' )  {
+    consola . log ( "Verdade! O tipo de dado da variável 'e' é uma string" ) ;
+}  senão  {
+    consola . log ( "Falso! O tipo de dado da variável 'e' não é uma string" ) ;
